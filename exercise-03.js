b62e3a420f5913efc7ad7f51a25554cf05463d4a
joana sousa <75800825+Joanadekastro@users.noreply.github.com>
2022-02-07 15:08:36 +0000

    exercise 3

---
+++ exercise-03.js
@@ -0,0 +1,12 @@
+/*03) Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.*/
+
+let base;
+let exp;
+
+function result(base, exp) {
+
+	console.log(base ** exp);
+
+}
+
+result(4, 2)
