b20a17d95e1e048007230d702a58ab16ff3b5656
joana sousa <75800825+Joanadekastro@users.noreply.github.com>
2022-02-07 15:26:58 +0000

    exercise 4

---
+++ exercise-4.js
@@ -0,0 +1,12 @@
+/*04) Crie uma função que irá receber dois valores, o dividendo e o divisor. A função deverá imprimir 
+o resultado e o resto da divisão destes dois valores.*/
+
+let dividendo;
+let divisor;
+
+function resultado(dividendo, divisor) {
+
+	console.log("resultado =" + (dividendo / divisor), "resto da divisao =" + (dividendo % divisor));
+}
+
+resultado(5, 2)
